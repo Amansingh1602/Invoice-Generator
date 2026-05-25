cd080c1e3b79b748d9507e0ad795d39be2d76d18
Ankit <ankitshankhdhar694@gmail.com>
2026-05-25 21:22:17 +0530

    Initial commit: Invoice Generator project with SRS deliverable

---
+++ routes/invoice.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\Invoice;
+use App\Services\InvoicePdfService;
+use Illuminate\Support\Facades\Route;
+
+// we'll add our public invoice routes here
+
+Route::livewire('create-invoice', 'pages::invoice.create')
+->name('create-invoice');
+
+Route::get('/invoice/{invoice}/download', function (Invoice $invoice) {
+    // Check authorization
+    if ($invoice->user_id !== Auth::id()) {
+        abort(403);
+    }
+    
+    $pdfService = new InvoicePdfService();
+    return $pdfService->download($invoice);
+})->middleware('auth')->name('invoice.download');
+
+Route::get('/invoice/{invoice}/print', function (Invoice $invoice) {
+    // Check authorization
+    if ($invoice->user_id !== Auth::id()) {
+        abort(403);
+    }
+    
+    return view('invoice-print', compact('invoice'));
+})->middleware('auth')->name('invoice.print');
